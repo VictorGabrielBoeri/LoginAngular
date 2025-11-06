9004eb2c575fda3ec011452b8862589e8352deea
Victor Gabriel de Oliveira Boeri <victorgabrielboeri10@gmail.com>
2025-11-06 15:14:53 -0300

    feat: signup page

---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,67 @@
+import { routes } from '../../app.routes';
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Route, Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
+
+interface signupForm {
+  name: FormControl,
+  email: FormControl,
+  password: FormControl,
+  passwordConfirm: FormControl,
+}
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent,
+  ],
+  providers: [LoginService],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss',
+})
+export class SignupComponent {
+  signupForm!: FormGroup<signupForm>;
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastr: ToastrService
+  ) {
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      passwordConfirm: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+
+  submit() {
+    this.loginService
+      .login(this.signupForm.value.email, this.signupForm.value.password)
+      .subscribe({
+        next: () => this.toastr.success('Login feito com sucesso'),
+        error: () => this.toastr.error('Erro tente novamente'),
+      });
+  }
+
+  navigate() {
+    this.router.navigate(['/login']);
+  }
+}
